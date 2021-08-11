b82ac322c0b19ad7240f9f686f0c090803c57a18
ShubhamAr-TI <shubham.arawkar@trilogy.com>
2021-08-11 17:26:03 +0000

    removed unnecessary import

--- restapi/tasks.py
+++ restapi/tasks.py
@@ -2,8 +2,7 @@ from celery import shared_task
 import os
 import boto3
 from subprocess import call
-import urllib
-import requests
+import urllibg
 
 @shared_task(name="bulk_expense_insert")
 def bulk_expenses(data):
