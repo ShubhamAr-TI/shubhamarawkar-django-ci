8e376a2d34600bbe90733c7d3d77261309970160
Shubham Arawkar <shubham.arawkar@trilogy.com>
2021-08-19 12:15:15 +0530

    added codejudge files back

--- cjapp/settings.py
+++ cjapp/settings.py
@@ -136,6 +136,8 @@ STATIC_URL = '/static/'
 
 BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+# BROKER_URL = 'redis://redisserver:6379/0'
+# CELERY_RESULT_BACKEND = 'redis://redisserver:6379/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
